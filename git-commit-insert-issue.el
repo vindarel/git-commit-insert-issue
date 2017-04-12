1c1c58673c320e0863f7a0b6bca8bae9d68c40eb
Tjaart van der Walt <git@tjaart.co.za>
2017-04-11 21:19:49 -0500

    just realized I forgot to add the dependency

--- git-commit-insert-issue.el
+++ git-commit-insert-issue.el
@@ -6,7 +6,7 @@
 ;; URL: https://gitlab.com/emacs-stuff/git-commit-insert-issue/
 ;; Keywords: git, github, gitlab, bitbucket, commit, issues
 ;; Version: 0.2.0
-;; Package-Requires: ((helm "0") (projectile "0") (s "0") (github-issues "0") (gitlab "0"))
+;; Package-Requires: ((helm "0") (projectile "0") (s "0") (github-issues "0") (gitlab "0") (bitbucket "0"))
 ;; Summary: Get issues list when typeng "Fixes #" in a commit message. github, gitlab and bitbucket.
 
 ;; This file is NOT part of GNU Emacs.
